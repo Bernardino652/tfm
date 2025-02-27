7f8b17e175878f09355ef031e9cc8ecee6eaa919
Bernardino652 <bernardo.chancusig@epn.edu.ec>
2025-02-27 11:15:41 -0500

    Add files via upload

--- Scripts/alemania.py
+++ Scripts/alemania.py
@@ -18,7 +18,7 @@ El script crea un archivo CSV con información detallada sobre:
 - Información sobre vuelos (cuando está disponible)
 
 Los datos generados se basan en estadísticas reales pero incluyen
-elementos simuklados para crear un conjunto de datos completo y coherente.
+elementos simulados para crear un conjunto de datos completo y coherente.
 
 Autor: Bernardino Chancusig Espin
 Fecha: 25/02/2025
